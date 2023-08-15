baa59e7e1328f5051b849c0fd942c39be14de0b6
Kumar Sahil <2270168@kiit.ac.in>
2023-08-15 14:55:56 +0530

    new day

---
+++ Travel Management System/src/travel/management/system/Conn.java
@@ -0,0 +1,17 @@
+package travel.management.system;
+import java.sql.*;
+
+public class Conn {
+   Connection c;
+   Statement s;
+    Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///travelmanagementsystem","root","kumarsahil");
+            s = c.createStatement();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+   } 
+}
--- Travel Management System/src/travel/management/system/Signup.java
+++ Travel Management System/src/travel/management/system/Signup.java
@@ -2,10 +2,14 @@
 package travel.management.system;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.border.*;
+
         
-public class Signup extends JFrame {
-    
+public class Signup extends JFrame implements ActionListener{
+    JButton create, back;
+    JTextField tu1,pn1,tn1,tfanswer,an1;
+    Choice security;
     Signup(){
         setBounds(350,200,900,360);
         getContentPane().setBackground(Color.WHITE);
@@ -21,10 +25,10 @@ public class Signup extends JFrame {
         l1.setBounds(50,20,125,25);
         p1.add(l1);
         
-        JTextField t1 = new JTextField();
-        t1.setBounds(190,20,190,25);
-        t1.setBorder(BorderFactory.createEmptyBorder());
-        p1.add(t1);
+        tu1 = new JTextField();
+        tu1.setBounds(190,20,190,25);
+        tu1.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tu1);
         
         /////////////////////////////////////////////////////
         
@@ -33,7 +37,7 @@ public class Signup extends JFrame {
         ln1.setBounds(50,60,125,25);
         p1.add(ln1);
         
-        JTextField tn1 = new JTextField();
+        tn1 = new JTextField();
         tn1.setBounds(190,60,190,25);
         tn1.setBorder(BorderFactory.createEmptyBorder());
         p1.add(tn1);
@@ -45,7 +49,7 @@ public class Signup extends JFrame {
         pa1.setBounds(50,100,125,25);
         p1.add(pa1);
         
-        JTextField pn1 = new JTextField();
+        pn1 = new JTextField();
         pn1.setBounds(190,100,190,25);
         pn1.setBorder(BorderFactory.createEmptyBorder());
         p1.add(pn1);
@@ -57,7 +61,7 @@ public class Signup extends JFrame {
         security1.setBounds(50,140,125,25);
         p1.add(security1);
         
-        Choice security = new Choice();
+        security = new Choice();
         security.add("Fav Character from the Boys");
         security.add("Fav Marvel superhero");
         security.add("Your Lucky number");
@@ -72,26 +76,28 @@ public class Signup extends JFrame {
         a1.setBounds(50,180,125,25);
         p1.add(a1);
         
-        JTextField an1 = new JTextField();
+        an1 = new JTextField();
         an1.setBounds(190,180,190,25);
         an1.setBorder(BorderFactory.createEmptyBorder());
         p1.add(an1);
         
-        JButton create = new JButton("Create");
+        create = new JButton("Create");
         create.setBackground(Color.WHITE);
         create.setForeground(new Color(133,193,233));
         create.setFont(new Font("TAHOMA", Font.BOLD,14));
         create.setBounds(80,250,100,30);
+        create.addActionListener(this);
         p1.add(create);
         
         ////////////////////////////////////////////////////
         
-        JButton back = new JButton("Back");
-        back.setBackground(Color.WHITE);
+        back = new JButton("Back");
+	back.addActionListener(this);
+	back.setFont(new Font("TAHOMA", Font.BOLD,14));
+	back.setBounds(250,250,100,30);
+	back.setBackground(Color.WHITE);
         back.setForeground(new Color(133,193,233));
-        back.setFont(new Font("TAHOMA", Font.BOLD,14));
-        back.setBounds(250,250,100,30);
-        p1.add(back);
+	p1.add(back);
         
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource(("icons/signup.png")));
         Image i2 = i1.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
@@ -103,6 +109,32 @@ public class Signup extends JFrame {
         setVisible(true);
     }
     
+    public void actionPerformed(ActionEvent ac){
+        if(ac.getSource() == create){
+           String username =  tu1.getText();
+           String name =  tn1.getText();
+           String password =  pn1.getText();
+           String question =  security.getSelectedItem();
+           String answer =  an1.getText();
+            
+           String query = "Insert into account values('"+username+"','"+name+"', '"+password+"', '"+question+"', '"+answer+"' )";
+           try{
+               Conn c = new Conn();
+               c.s.executeUpdate(query);
+               
+               JOptionPane.showMessageDialog(null, "Account Created Successfully");
+               setVisible(false);
+               new Login();
+               
+           }catch(Exception e){
+               e.printStackTrace();
+           }
+        }
+        else if(ac.getSource() == back){
+           this.setVisible(false);
+            new Login().setVisible(true);
+        }
+    }
     
     public static void main(String[] args){
         new Signup();
